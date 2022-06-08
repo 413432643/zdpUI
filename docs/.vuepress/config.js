b1c204c7c3d89dcb817f64f9bec46c2637492b25
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-08 23:34:16 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,10 @@
 const { defaultTheme } = require('@vuepress/theme-default')
 
 module.exports = {
+  base: "/zdpUI/",
   theme: defaultTheme({
     // 在这里进行配置
-    base: "./zzzUI/",
+    
 
     sidebar: [
         
