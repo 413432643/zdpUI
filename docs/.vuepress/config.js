f84237c44f8e4e6f86f3e38217fd0250e64ede11
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-08 22:51:04 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,25 +3,9 @@ const { defaultTheme } = require('@vuepress/theme-default')
 module.exports = {
   theme: defaultTheme({
     // 在这里进行配置
-    base: "./zdpUI/",
+    base: "/zdpUI/",
     sidebar: [
-      // SidebarItem
-      // {
-      //   text: 'Foo',
-      //   link: '/foo/',
-      //   children: [
-      //     // SidebarItem
-      //     {
-      //       text: 'github',
-      //       link: 'https://github.com',
-      //       children: [],
-      //     },
-      //     // 字符串 - 页面文件路径
-      //     '/foo/bar.md',
-      //   ],
-      // },
-      '/',
-      // 'components/button.md'
+        
 
     ],
 
