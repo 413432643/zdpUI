3193b93e0e476ace6abe0aacf62aacc2d3456306
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-09 20:32:41 +0800

    test

--- docs/.vuepress/.temp/pages/componentDocs/checkbox.html.vue
+++ docs/.vuepress/.temp/pages/componentDocs/checkbox.html.vue
@@ -1,4 +1,5 @@
 <template><div><h1 id="checkbox-多选" tabindex="-1"><a class="header-anchor" href="#checkbox-多选" aria-hidden="true">#</a> checkbox 多选</h1>
+<z-button></z-button>111
 <details class="custom-container details"><summary>点击查看代码</summary>
 <div class="language-text ext-text line-numbers-mode"><pre v-pre class="language-text"><code>这是一个 details 标签
 </code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></details>
--- docs/componentDocs/checkbox.md
+++ docs/componentDocs/checkbox.md
@@ -1,5 +1,7 @@
 # checkbox 多选
 
+<z-button></z-button>111
+
 ::: details 点击查看代码
 ```
 这是一个 details 标签
