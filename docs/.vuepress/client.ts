04afe08804c8ec7bc80fe51d781ce33d56cdc6dd
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-10 17:14:15 +0800

    test

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,13 +2,13 @@ import { defineClientConfig } from '@vuepress/client'
 
 // import ZUI from 'zdp-ui'
 // import "zdp-ui/dist/style.css"
-import ElementPlus from 'element-plus'
+import {ElButton} from 'element-plus'
 import 'element-plus/dist/index.css'
 
 export default defineClientConfig({
   enhance({ app }) {
     // app.use(ZUI)
-    app.use(ElementPlus)
+    app.use(ElButton)
   },
 })
 
