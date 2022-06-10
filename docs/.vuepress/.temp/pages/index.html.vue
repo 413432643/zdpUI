7c1cd0403661048207c4dcfa22fa8d0917ba8e89
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-10 16:05:55 +0800

    test

--- docs/.vuepress/.temp/pages/index.html.vue
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -3,12 +3,12 @@
 <div class="language-bash ext-sh line-numbers-mode"><pre v-pre class="language-bash"><code><span class="token function">npm</span> i zdp-ui
 </code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h3 id="引用组件库" tabindex="-1"><a class="header-anchor" href="#引用组件库" aria-hidden="true">#</a> 引用组件库</h3>
 <div class="language-javascript ext-js line-numbers-mode"><pre v-pre class="language-javascript"><code><span class="token comment">// 全部引用</span>
-<span class="token keyword">import</span> <span class="token string">'zdp-ui/dist/assets/index.73364d44.css'</span>
+<span class="token keyword">import</span> <span class="token string">'zdp-ui/dist/style.css'</span>
 <span class="token keyword">import</span> <span class="token constant">ZUI</span> <span class="token keyword">from</span> <span class="token string">'zdp-ui'</span>
 Vue<span class="token punctuation">.</span><span class="token function">use</span><span class="token punctuation">(</span><span class="token constant">ZUI</span><span class="token punctuation">)</span>
 
 <span class="token comment">// 按需引用</span>
-<span class="token keyword">import</span> <span class="token string">'zdp-ui/dist/assets/button.css'</span>
+<span class="token keyword">import</span> <span class="token string">'zdp-ui/dist/style.css'</span>
 <span class="token keyword">import</span> <span class="token punctuation">{</span>Button<span class="token punctuation">}</span> <span class="token keyword">from</span> <span class="token string">'zdp-ui'</span>
 Vue<span class="token punctuation">.</span><span class="token function">use</span><span class="token punctuation">(</span>Button<span class="token punctuation">)</span>
 
--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,14 +1,14 @@
 import { defineClientConfig } from '@vuepress/client'
 
-import ZUI from 'zdp-ui'
+// import ZUI from 'zdp-ui'
 // import "zdp-ui/dist/style.css"
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
+import {ElementPlus} from 'element-plus'
+import 'element-plus/dist/index.css'
 
 export default defineClientConfig({
   enhance({ app }) {
-    app.use(ZUI)
-    // app.use(ElementPlus)
+    // app.use(ZUI)
+    app.use(ElementPlus)
   },
 })
 
