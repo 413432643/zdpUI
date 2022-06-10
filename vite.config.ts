3d0996737e35f4c70f839318eea6c2854d518bb3
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-10 16:51:08 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import alias from "@rollup/plugin-alias";
-import { resolve } from "path";
+import  path  from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [alias(),vue()],
   resolve: {
     alias: {
-      "/@": resolve(__dirname, "./src"),
+      "/@": path.resolve(__dirname, "./src"),
     },
   },
   build:{
