16d99d926bfb8b5102270b3b279c594d6a925f97
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-06-10 17:04:27 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import  path  from 'path';
-import resolve from "rollup-plugin-node-resolve";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      "/@": path.resolve(__dirname, "./src"),
-    },
-  },
   build:{
     // 不需要的外部依赖
     rollupOptions:{
@@ -20,9 +14,6 @@ export default defineConfig({
           vue:"Vue"
         }
       },
-      plugins:[
-        resolve()
-      ]
     },
     lib:{
       // 入口
