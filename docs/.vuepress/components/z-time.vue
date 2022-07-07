ca76f529882bfc8493d982f4dc850c40ad01a5ca
413432643 <9022695+zdp12345@user.noreply.gitee.com>
2022-07-07 16:26:25 +0800

    'time'

--- docs/.vuepress/components/z-time.vue
+++ docs/.vuepress/components/z-time.vue
@@ -3,24 +3,29 @@
         <z-input class="input" leftIcon="icon-time" clearable @focus="focus"></z-input>
         <div class="time" v-if="timeShow">
             <div class="time-body">
-                <div class="hour">
-                    <div v-for="h in 24" :key="h">{{ h }}</div>
+                <div class="hour" @scroll="itemScroll">
+                    <div v-for="h in 24" :key="h" :class="hClass(h)" @click="timeClick(h, $event)">{{ (h < 11 ? "0" : "")
+                            + (h - 1)
+                    }} </div>
+                    </div>
+                    <div class="minute" @scroll="itemScroll">
+                        <div v-for="m in 60" :key="m" :class="mClass(m)" @click="timeClick(m, $event)">{{ (m < 11 ? "0"
+                                : "") + (m - 1)
+                        }} </div>
+                        </div>
+                        <div class="second" @scroll="itemScroll">
+                            <div v-for="s in 60" :key="s" :class="sClass(s)" @click="timeClick(s, $event)">{{ (s < 11 ? "0"
+                                    : "") + (s - 1)
+                            }} </div>
+                            </div>
+                        </div>
+                        <div class="selectRegion"></div>
+                        <div class="time-foot">
+                            <div class="cancel" @click="cancel">取消</div>
+                            <div class="ok" @click="ok">完成</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="minute">
-                    <div v-for="m in 60" :key="m">{{ m }}</div>
-                </div>
-                <div class="second">
-                    <div v-for="s in 60" :key="s">{{ s }}</div>
-                </div>
-            </div>
-
-            <div class="time-foot">
-                <div class="cancel" @click="cancel">取消</div>
-                <div class="ok" @click="ok">完成</div>
-            </div>
-        </div>
-
-    </div>
 </template>
 
 
@@ -42,6 +47,10 @@ const props = defineProps({
 
 const timeShow = ref(false)
 
+const hSelect = ref(1)
+const mSelect = ref(1)
+const sSelect = ref(1)
+
 const focus = () => {
     timeShow.value = true
 }
@@ -57,6 +66,53 @@ const vDown = { //元素外收起
         document.addEventListener('click', hander)
     }
 }
+
+const hClass = computed(() => (num) => {
+    return [
+        hSelect.value == num ? 'select' : '',
+    ]
+})
+const mClass = computed(() => (num) => {
+    return [
+        mSelect.value == num ? 'select' : '',
+    ]
+})
+const sClass = computed(() => (num) => {
+    return [
+        sSelect.value == num ? 'select' : '',
+    ]
+})
+
+
+
+const itemScroll = (e) => {
+    let t = null
+    return function () {
+        if (t != null) clearTimeout(t)
+        t = setTimeout(() => {
+            // 滚动30的倍数
+            let top = e.target.scrollTop
+            e.target.scrollTop = top - top % 30
+            // 到指定高度的元素
+            let className = e.path[0].className
+            let length = e.path[0].children.length
+            for (let i = 0; i < length; i++) {
+                let childrenTop = e.path[0].children[0].offsetTop - 90 + 30 * i
+                if (top == childrenTop) {
+                    if (className == 'hour') hSelect.value = i + 1
+                    if (className == 'minute') mSelect.value = i + 1
+                    if (className == 'second') sSelect.value = i + 1
+                }
+            }
+        }, 500)
+    }()
+}
+
+const timeClick = (h, e) => {
+    let parentTop = e.path[1].scrollTop
+    e.path[1].scrollTop += ((h - 1) * 30 - parentTop)
+}
+
 
 const cancel = () => {
 
@@ -83,14 +139,11 @@ const ok = () => {
         background: #fff;
         border-radius: 4px;
         box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-        z-index: 999;
-
+        z-index: 9;
 
         &-body {
-
             width: 100%;
             max-height: 200px;
-
             display: flex;
 
             .hour,
@@ -98,7 +151,8 @@ const ok = () => {
             .second {
                 width: 33.33%;
                 overflow-y: scroll;
-                
+                padding: 90px 0;
+
                 div {
                     font-size: 12px;
                     text-align: center;
@@ -107,7 +161,8 @@ const ok = () => {
                     color: #606266;
                     cursor: pointer;
                 }
-                :hover{
+
+                :hover {
                     background: #eee;
                 }
 
@@ -130,16 +185,13 @@ const ok = () => {
                 }
             }
 
+            // .hour {}
 
-            .hour {}
+            // .minute {}
 
-            .minute {}
-
-            .second {}
-
+            // .second {}
 
         }
-
 
         &-foot {
             display: flex;
@@ -147,9 +199,10 @@ const ok = () => {
             justify-content: right;
             padding: 8px 10px;
             border-top: 1px solid #eee;
+
             .cancel,
             .ok {
-                padding:0 5px;
+                padding: 0 5px;
                 cursor: pointer;
             }
 
@@ -158,5 +211,20 @@ const ok = () => {
             }
         }
     }
+}
+
+.select {
+    font-weight: 900
+}
+
+.selectRegion {
+    top: 90px;
+    position: absolute;
+    border-top: 1px solid #eee;
+    border-bottom: 1px solid #eee;
+    z-index: 999;
+    height: 30px;
+    width: 150px;
+    margin: 0 15px;
 }
 </style>
